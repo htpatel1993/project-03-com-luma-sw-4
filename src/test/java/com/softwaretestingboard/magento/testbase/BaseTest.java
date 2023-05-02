4a4a3d12fd009adace2817e0cf0d697422e47126
htpatel1993 <htpatel1993@gmail.com>
2023-05-02 19:21:48 +0100

    Initial commit

---
+++ src/test/java/com/softwaretestingboard/magento/testbase/BaseTest.java
@@ -0,0 +1,19 @@
+package com.softwaretestingboard.magento.testbase;
+
+import com.softwaretestingboard.magento.utilities.Utility;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest extends Utility {
+    String browser = "Chrome";
+    @BeforeMethod
+    public void setup(){
+
+        selectBrowser(browser);
+    }
+    @AfterMethod
+    public void tearDown(){
+        closeBrowser();
+
+    }
+}
